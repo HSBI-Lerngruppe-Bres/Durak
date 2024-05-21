1490980aabb38036be30872bba8cf5031f7e27cc
Benjamin Albrecht <physik.chemie.mathe.bio@gmail.com>
2024-05-21 18:20:26 +0200

    Definition der Datenbank

---
+++ models.py
@@ -0,0 +1,28 @@
+# models.py
+from werkzeug.security import generate_password_hash, check_password_hash
+from database import db
+
+class Spieler(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Spiel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    startzeit = db.Column(db.DateTime, nullable=False)
+
+class SpielZustand(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    spielsitzung_id = db.Column(db.String(36), unique=True, nullable=False)
+    spieler_haende = db.Column(db.String(500), nullable=False)
+    aktueller_spieler_id = db.Column(db.String(50), nullable=False)
+    ablage_stapel = db.Column(db.String(500), nullable=False)
+    zieh_stapel = db.Column(db.String(500), nullable=False)
+    trumpf = db.Column(db.String, nullable=False) 
