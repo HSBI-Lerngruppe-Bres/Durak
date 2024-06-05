554fd70b575ee98c28dd9252dcd19d83200236ae
Elina <elina.baisariev@web.de>
2024-06-05 18:15:30 +0200

    Spielstart, Ablage Anfang

--- static/spielfeld.js
+++ static/spielfeld.js
@@ -27,6 +27,9 @@ socketio.on("update_deck", (data) => {
   deck.forEach(card => {
     const cardDiv = document.createElement('div');
     cardDiv.className = 'card';
+    cardDiv.setAttribute('data-rank', card.rank);
+    cardDiv.setAttribute('data-suit', card.suit);
+    cardDiv.addEventListener('click', () => playCard(card.rank, card.suit)); // Click event to play the card
     const img = document.createElement('img');
     img.src = `/static/svg/${card.rank}${card.suit}.svg`;
     img.alt = `${card.rank} of ${card.suit}`;
@@ -34,6 +37,10 @@ socketio.on("update_deck", (data) => {
     deckDiv.appendChild(cardDiv);
   });
 });
+
+const playCard = (rank, suit) => {
+  socketio.emit('play_card', { rank, suit });
+};
 
 document.getElementById('copy-btn').addEventListener("click", function() {
   const textarea = document.getElementById('code-textarea');
@@ -50,4 +57,45 @@ document.getElementById('copy-btn').addEventListener("click", function() {
   } catch (err) {
       alert('Fehler beim Kopieren des Textes: ' + err);
   }
+});
+
+document.getElementById('start-game-btn').addEventListener("click", function() {
+  socketio.emit('start_game');
+});
+
+socketio.on('redirect', (data) => {
+  window.location.href = data.url + "?error=The game has already started, please choose another Gameroom or create a new one.";
+});
+
+socketio.on('start_player', (data) => {
+  const startPlayer = data.player;
+  createMessage('System', `${startPlayer} beginnt das Spiel.`);
+});
+
+socketio.on('card_played', (data) => {
+  const { rank, suit, player } = data;
+  createMessage('System', `${player} hat die Karte ${rank} of ${suit} gespielt.`);
+  const playedCardsDiv = document.getElementById('played-cards');
+  const cardDiv = document.createElement('div');
+  cardDiv.className = 'card';
+  const img = document.createElement('img');
+  img.src = `/static/svg/${rank}${suit}.svg`;
+  img.alt = `${rank} of ${suit}`;
+  cardDiv.appendChild(img);
+  playedCardsDiv.appendChild(cardDiv);
+});
+
+socketio.on('update_played_cards', (data) => {
+  const playedCards = data.played_cards;
+  const playedCardsDiv = document.getElementById('played-cards');
+  playedCardsDiv.innerHTML = ''; // Clear the div first
+  playedCards.forEach(card => {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+    const img = document.createElement('img');
+    img.src = `/static/svg/${card.rank}${card.suit}.svg`;
+    img.alt = `${card.rank} of ${card.suit}`;
+    cardDiv.appendChild(img);
+    playedCardsDiv.appendChild(cardDiv);
+  });
 });
